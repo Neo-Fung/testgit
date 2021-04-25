94b13abd7dd5549c0af3046575531fc22ba9982a
neo <1934235919@qq.com>
2021-04-25 21:18:17 +0800

    更新3+新增db密码

--- hellogit/src/main/java/com/HelloGit.java
+++ hellogit/src/main/java/com/HelloGit.java
@@ -5,5 +5,6 @@ public class HelloGit {
         System.out.println("hello git");
         System.out.println("update 1");
         System.out.println("update 2");
+        System.out.println("update 3");
     }
 }
